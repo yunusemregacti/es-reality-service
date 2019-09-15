cbf7d6cddeef6f310ddc7bbf2e04b61e859341d3
Yunus Emre Gaçti <yunusemregacti@hotmail.com>
2019-09-15 19:24:14 +0300

    push to github

---
+++ buildlocal.sh
@@ -0,0 +1,3 @@
+rm -rf _builds _steps _projects _cache _temp
+wercker build --git-domain github.com --git-owner yunusemregacti --git-repository es-reality-service
+rm -rf _builds _steps _projects _cache _temp 
